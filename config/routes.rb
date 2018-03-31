e3542fc6d2bf0a239d950116a07bc55ed3c900d6
damianrivas <drivas1993@gmail.com>
2018-03-30 19:33:54 -0700

    Create API controller to record Events with cross-origin resource sharing

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,11 @@ Rails.application.routes.draw do
     resources :registered_applications
   end
 
+  namespace "api", defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
+
   root 'welcome#index'
 
   get '/about', to: 'welcome#about'
